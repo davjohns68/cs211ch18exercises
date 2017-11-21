6c2dbdeeddbf50e9d877675999a522f17aa11037
David Johnson <davjohns@outlook.com>
2017-11-21 11:06:09 -0800

    Initial commit

---
+++ src/cs211ch18exercises/Cs211ch18exercises.java
@@ -0,0 +1,21 @@
+/*
+ * <Enter Description>
+ * Created by David Johnson, <Date>
+ * for CS211 course, Bellevue College
+ */
+package cs211ch18exercises;
+
+/**
+ *
+ * @author davjohns
+ */
+public class Cs211ch18exercises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
